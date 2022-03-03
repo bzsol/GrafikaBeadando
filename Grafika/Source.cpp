489960a13bd7f4d65576e81e27eedc0c31492dec
bzsol <bereczzsolt2000@gmail.com>
2022-03-03 23:59:23 +0100

    Done First Project

--- Grafika/Source.cpp
+++ Grafika/Source.cpp
@@ -1,4 +1,4 @@
-﻿#include <GL/glew.h>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include "glm/glm.hpp"
@@ -10,46 +10,19 @@
 #include <vector>
 #include <time.h>
 #include <cmath>
-#define numVBO 4
-#define numVAO 2
+
 using namespace std;
 
-int			window_width = 600;
-int			window_height = 600;
-char		window_title[] = "Beadando 1.";
-GLFWwindow* window = nullptr;
 
-unsigned int VAO[numVAO];
-unsigned int VBO[numVBO];
-
-unsigned int shaderLineProgram;
-unsigned int shaderCircleProgram;
-
-GLuint		renderingProgram;
-GLuint		XoffsetLocation;
-GLuint		YoffsetLocation;
-
-float		x = 0.00f;
-float		y = 0.00f;
-float		increment = 0.01f;
-
-bool		xDir = true;
-bool		yDir = false;
-
-
-// These are the line vertices and the colors.
-float verticesLine[] = {
-	-0.33,0.0,0.0,
-	0.33,0.0,0.0
-};
-
-float LineColor[] = {
-	0,0,1,0,
-	0,0,1,0
+static GLfloat line_vertices[] =
+{
+	-0.33f, 0.0f, 0.00f,
+	 0.33f, 0.0f, 0.00f
 };
 
 std::vector<glm::vec3> verticesCircle;
 std::vector<glm::vec3> CircleColor;
+std::vector<glm::vec3> CircleColor2;
 
 
 // Create the lines for the vertices and also the colors (with glm)
@@ -57,85 +30,180 @@ void CreateCircleLinesandColors()
 {
 	for (int i = 0; i <= 360; i++)
 	{
-		float theta = (i * (atan(1) * 4) / 180);
-		float x = 0.16 * cosf(theta);
-		float y = 0.16 * sinf(theta);
-		float z = 0.0f;
+		GLdouble theta = (i * (atan(1) * 4) / 180);
+		GLdouble x = 0.16 * cosf(theta); // 0-1 300px 1/6 0,16 -> 50px
+		GLdouble y = 0.16 * sinf(theta); 
 		if (i % 2 == 0) {
-			verticesCircle.push_back(glm::vec3(x, y, z));
-			CircleColor.push_back(glm::vec3(0.0, 1.0, 0.0));
+			verticesCircle.push_back(glm::vec3(x, y, 0));
+			CircleColor2.push_back(glm::vec3(0.0, 0.5, 0.0));
+			CircleColor.push_back(glm::vec3(1.0, 0.0, 0.0));
 		}
 		else {
 			verticesCircle.push_back(glm::vec3(0, 0, 0));
-			CircleColor.push_back(glm::vec3(1.0, 0.0, 0.0));
+			CircleColor2.push_back(glm::vec3(1.0, 0.0, 0.0));
+			CircleColor.push_back(glm::vec3(0.0, 0.5, 0.0));
 		}
 
 	}
 }
 
-// Read the Shader file
-std::string readShaderSource(const char* filePath) {
-	string content;
-	ifstream fileStream(filePath, ios::in);
-	string line = "";
 
-	while (!fileStream.eof()) {
-		getline(fileStream, line);
-		content.append(line + "\n");
-	}
-	fileStream.close();
-	return content;
-}
+#define		numVBOs			3
+#define		numVAOs			2
+GLuint		VBO[numVBOs];
+GLuint		VAO[numVAOs];
 
+int			window_width	= 600;
+int			window_height	= 600;
+char		window_title[]	= "Beadando1";
+
+GLboolean	keyboard[512]	= {GL_FALSE};
+GLFWwindow	*window			= nullptr;
+GLuint		LineProgram;
+GLuint		CircleProgram;
+GLuint		XoffsetLocation;
+GLuint		YoffsetLocation;
+bool random = false;
+
+float		x				= 0.00f;
+float		y				= 0.00f;
+float		increment		= 0.01f;
+
+bool		xDir			= true;
+bool		yDir			= false;
 
 bool checkOpenGLError() {
-	bool	foundError = false;
-	int		glErr = glGetError();
+	bool	foundError	= false;
+	int		glErr		= glGetError();
 
 
 	while (glErr != GL_NO_ERROR) {
 		cout << "glError: " << glErr << endl;
 
-		foundError = true;
-		glErr = glGetError();
+		foundError	= true;
+		glErr		= glGetError();
 	}
+
 
 	return foundError;
 }
 
-void printShaderLog(unsigned int shader) {
-	int		length = 0;
-	int		charsWritten = 0;
-	char* log = nullptr;
-
+void printShaderLog(GLuint shader) {
+	int		length			= 0;
+	int		charsWritten	= 0;
+	char	*log			= nullptr;
+	
 	
 	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &length);
-
+	
 	if (length > 0) {
 		log = (char*)malloc(length);
+
+
 		glGetShaderInfoLog(shader, length, &charsWritten, log);
 		cout << "Shader Info Log: " << log << endl;
 		free(log);
 	}
 }
 
-
 void printProgramLog(int prog) {
-	int		length = 0;
-	int		charsWritten = 0;
-	char* log = nullptr;
-
+	int		length			= 0;
+	int		charsWritten	= 0;
+	char	*log			= nullptr;
+	
 	
 	glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &length);
-
+	
 	if (length > 0) {
 		log = (char*)malloc(length);
+
+
 		glGetProgramInfoLog(prog, length, &charsWritten, log);
 		cout << "Program Info Log: " << log << endl;
 		free(log);
 	}
 }
 
+string readShaderSource(const char* filePath) {
+
+	ifstream	fileStream(filePath, ios::in);
+	string		content;
+	string		line;
+
+
+	while (!fileStream.eof()) {
+		getline(fileStream, line);
+		content.append(line + "\n");
+	}
+
+
+	fileStream.close();
+	
+
+	return content;
+}
+
+GLuint createShaderProgramforLine() {
+	GLint		vertCompiled;
+	GLint		fragCompiled;
+	GLint		linked;
+
+	string		vertShaderStr = readShaderSource("vertexShader.glsl");
+	string		fragShaderStr = readShaderSource("fragmentShader.glsl");
+
+	GLuint		vShader = glCreateShader(GL_VERTEX_SHADER);
+	GLuint		fShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	const char	*vertShaderSrc = vertShaderStr.c_str();
+	const char	*fragShaderSrc = fragShaderStr.c_str();
+
+
+	glShaderSource(vShader, 1, &vertShaderSrc, NULL);
+	glShaderSource(fShader, 1, &fragShaderSrc, NULL);
+
+
+	glCompileShader(vShader);
+
+	checkOpenGLError();
+	glGetShaderiv(vShader, GL_COMPILE_STATUS, &vertCompiled);
+	if (vertCompiled != 1) {
+		cout << "Vertex compilation failed." << endl;
+		printShaderLog(vShader);
+	}
+
+
+	glCompileShader(fShader);
+
+	checkOpenGLError();
+	glGetShaderiv(fShader, GL_COMPILE_STATUS, &fragCompiled);
+	if (fragCompiled != 1) {
+		cout << "Fragment compilation failed." << endl;
+		printShaderLog(fShader);
+	}
+
+
+	GLuint		vfProgram = glCreateProgram();
+
+	glAttachShader(vfProgram, vShader);
+	glAttachShader(vfProgram, fShader);
+
+
+	glLinkProgram(vfProgram);
+
+	checkOpenGLError();
+	glGetProgramiv(vfProgram, GL_LINK_STATUS, &linked);
+	if (linked != 1) {
+		cout << "Shader linking failed." << endl;
+		printProgramLog(vfProgram);
+	}
+
+
+	glDeleteShader(vShader);
+	glDeleteShader(fShader);
+
+
+	return vfProgram;
+}
 
 GLuint createShaderProgramforCircle() {
 	GLint		vertCompiled;
@@ -160,77 +228,6 @@ GLuint createShaderProgramforCircle() {
 
 	glCompileShader(vShader);
 
-
-	checkOpenGLError();
-	glGetShaderiv(vShader, GL_COMPILE_STATUS, &vertCompiled);
-	if (vertCompiled != 1) {
-		cout << "Vertex compilation failed." << endl;
-		printShaderLog(vShader);
-	}
-
-	
-	glCompileShader(fShader);
-	
-
-	checkOpenGLError();
-	glGetShaderiv(fShader, GL_COMPILE_STATUS, &fragCompiled);
-	if (fragCompiled != 1) {
-		cout << "Fragment compilation failed." << endl;
-		printShaderLog(fShader);
-	}
-
-	GLuint		vfProgram = glCreateProgram();
-	
-
-	glAttachShader(vfProgram, vShader);
-	glAttachShader(vfProgram, fShader);
-
-
-	glLinkProgram(vfProgram);
-	
-
-	checkOpenGLError();
-	glGetProgramiv(vfProgram, GL_LINK_STATUS, &linked);
-	if (linked != 1) {
-		cout << "Shader linking failed." << endl;
-		printProgramLog(vfProgram);
-	}
-
-	
-
-	glDeleteShader(vShader);
-	glDeleteShader(fShader);
-
-	
-	return vfProgram;
-}
-
-
-
-GLuint createShaderProgramforLine() {
-	GLint		vertCompiled;
-	GLint		fragCompiled;
-	GLint		linked;
-
-	string		vertShaderStr = readShaderSource("vertexShaderLine.glsl");
-	string		fragShaderStr = readShaderSource("fragmentShaderLine.glsl");
-
-
-	GLuint		vShader = glCreateShader(GL_VERTEX_SHADER);
-	GLuint		fShader = glCreateShader(GL_FRAGMENT_SHADER);
-
-
-	const char* vertShaderSrc = vertShaderStr.c_str();
-	const char* fragShaderSrc = fragShaderStr.c_str();
-
-
-	glShaderSource(vShader, 1, &vertShaderSrc, NULL);
-	glShaderSource(fShader, 1, &fragShaderSrc, NULL);
-
-
-	glCompileShader(vShader);
-
-
 	checkOpenGLError();
 	glGetShaderiv(vShader, GL_COMPILE_STATUS, &vertCompiled);
 	if (vertCompiled != 1) {
@@ -241,7 +238,6 @@ GLuint createShaderProgramforLine() {
 
 	glCompileShader(fShader);
 
-
 	checkOpenGLError();
 	glGetShaderiv(fShader, GL_COMPILE_STATUS, &fragCompiled);
 	if (fragCompiled != 1) {
@@ -249,15 +245,14 @@ GLuint createShaderProgramforLine() {
 		printShaderLog(fShader);
 	}
 
-	GLuint		vfProgram = glCreateProgram();
 
+	GLuint		vfProgram = glCreateProgram();
 
 	glAttachShader(vfProgram, vShader);
 	glAttachShader(vfProgram, fShader);
 
 
 	glLinkProgram(vfProgram);
-
 
 	checkOpenGLError();
 	glGetProgramiv(vfProgram, GL_LINK_STATUS, &linked);
@@ -267,198 +262,240 @@ GLuint createShaderProgramforLine() {
 	}
 
 
-
 	glDeleteShader(vShader);
 	glDeleteShader(fShader);
-
 
 	return vfProgram;
 }
 
 void init(GLFWwindow* window) {
 
+	LineProgram = createShaderProgramforLine();
+	CircleProgram = createShaderProgramforCircle();
+
+
+	glGenBuffers(numVBOs, VBO);
+	glGenVertexArrays(numVAOs, VAO);
+
+
 	// Circle
 	CreateCircleLinesandColors();
-
-	glGenBuffers(numVBO, VBO);
-	glGenVertexArrays(numVAO, VAO);
+	
+	glBindVertexArray(VAO[0]);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
 
 	glBufferData(GL_ARRAY_BUFFER, verticesCircle.size() * sizeof(glm::vec3), verticesCircle.data(), GL_STATIC_DRAW);
-	glBindVertexArray(VAO[0]);
 
-	glEnableVertexAttribArray(0);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
 
 	glGenBuffers(1, &VBO[1]);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
 
 	glBufferData(GL_ARRAY_BUFFER, CircleColor.size() * sizeof(glm::vec3), CircleColor.data(), GL_STATIC_DRAW);
 
-	glEnableVertexAttribArray(1);
+
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 
-	/* Engedélyezzük az imént definiált location = 0 attribútumot (vertexShader.glsl). */
-	/* Enable the previously defined location = 0 attributum (vertexShader.glsl). */
-
-
-	/* Leválasztjuk a vertex array objektumot és a buffert is. */
-	/* Detach the vertex array object and the buffer also. */
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	
+	//Line
 
-
-	// Line
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO[2]);
-
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesLine), verticesLine, GL_STATIC_DRAW);
 	glBindVertexArray(VAO[1]);
 
-
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[2]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(line_vertices), line_vertices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-
-	/* Engedélyezzük az imént definiált location = 0 attribútumot (vertexShader.glsl). */
-	/* Enable the previously defined location = 0 attributum (vertexShader.glsl). */
-	glBindBuffer(GL_ARRAY_BUFFER, VBO[3]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(LineColor), LineColor, GL_STATIC_DRAW);
-	glBindVertexArray(VAO[1]);
-	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(1);
-	/* Leválasztjuk a vertex array objektumot és a buffert is. */
-	/* Detach the vertex array object and the buffer also. */
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	shaderCircleProgram = createShaderProgramforCircle();
-	shaderLineProgram = createShaderProgramforLine();
-
-	
-	
-
+	glClearColor(0.5, 0.5, 0.0, 1.0);
 }
 
-/** A kód, amellyel rajzolni tudunk a GLFWwindow objektumunkba. */
-/** Call display function which will draw into the GLFWwindow object. */
-void display() {
-	/** Töröljük le a színbuffert! */
-	/** Let's clear the color buffer! */
-	glClearColor(0.7, 0.7, 0.0, 1.0);
-	glClear(GL_COLOR_BUFFER_BIT);
+void display(GLFWwindow* window, double currentTime) {
 
-	//Line 
-	glUseProgram(shaderLineProgram);
+	glClear(GL_COLOR_BUFFER_BIT); // fontos lehet minden egyes alkalommal t�r�lni!
 
-	glBindVertexArray(VAO[1]);
-	glLineWidth(3);
-	glDrawArrays(GL_LINE_LOOP, 0, sizeof(verticesLine) / sizeof(verticesLine[0]));
 
-	// Leválasztom
+
+
+	glUseProgram(0);
+	glUseProgram(LineProgram);
+ 	glBindVertexArray(VAO[1]);
+	glLineWidth(3); // vonal vastags�g 3px
+	glDrawArrays(GL_LINES, 0, 2);
 	glBindVertexArray(0);
-	
-	
-	// Circle
-	glUseProgram(shaderCircleProgram);
+
+
+	glUseProgram(0);
+	glUseProgram(CircleProgram);
 	glBindVertexArray(VAO[0]);
 	//glDrawArrays(GL_LINE_LOOP, 0, verticesCircle.size());
 	glDrawArrays(GL_TRIANGLE_STRIP, 0, verticesCircle.size());
-	
 	if (xDir) {
 		x += increment;
-		if (x > 0.84f) increment = -0.01f;
-		if (x < -0.84f) increment = 0.01f;
-		unsigned int offsetLoc = glGetUniformLocation(shaderCircleProgram, "offsetX");
-		glProgramUniform1f(shaderCircleProgram, offsetLoc, x);
+		if (x > 0.84f) {
+			increment = -0.01f;
+		}
+		if (x < -0.84f) {
+			increment = 0.01f;
+		}
+		GLuint offsetLoc = glGetUniformLocation(CircleProgram, "offsetX");
+		glProgramUniform1f(CircleProgram, offsetLoc, x);
 	}
 	if (yDir) {
 		y += increment;
-		if (y > 0.84f) increment = -0.01f;
+		if (y > 0.84f) increment = -0.01f; // 0.84 fogja a k�rvonal el�rni a sz�l�t
 		if (y < -0.84f) increment = 0.01f;
-		GLuint offsetLoc = glGetUniformLocation(shaderCircleProgram, "offsetY");
-		glProgramUniform1f(shaderCircleProgram, offsetLoc, y);
+		GLuint offsetLoc = glGetUniformLocation(CircleProgram, "offsetY");
+		glProgramUniform1f(CircleProgram, offsetLoc, y);
 	}
-	glBindVertexArray(0);
+	if ((y >= -0.12 && y <= 0.12) && (x >= -0.50 && x <= 0.50)) {
+		cout << "X: ";
+		cout << x << endl;
+		cout << "Y: ";
+		cout << y << endl;
+		glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+		glBufferData(GL_ARRAY_BUFFER, CircleColor2.size() * sizeof(glm::vec3), CircleColor2.data(), GL_STATIC_DRAW);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(1);
+		glBindVertexArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	}
+	else {
+		glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
 
+		glBufferData(GL_ARRAY_BUFFER, CircleColor.size() * sizeof(glm::vec3), CircleColor.data(), GL_STATIC_DRAW);
+
+
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+		glEnableVertexAttribArray(1);
+		glBindVertexArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	}
+
+
+	glBindVertexArray(0);
 }
 
-/** Felesleges objektumok törlése. */
-/** Clenup the unnecessary objects. */
+
 void cleanUpScene() {
 
-	glDeleteVertexArrays(1, &VAO[0]);
-	glDeleteBuffers(1, &VBO[0]);
-	glDeleteProgram(shaderCircleProgram);
+	glDeleteVertexArrays(numVAOs, VAO);
+	glDeleteBuffers(numVBOs, VBO);
 
-	glDeleteVertexArrays(1, &VAO[1]);
-	glDeleteBuffers(1, &VBO[1]);
-	glDeleteProgram(shaderLineProgram);
 
-	/** Töröljük a GLFW ablakot. */
-	/** Destroy the GLFW window. */
+	glDeleteProgram(LineProgram);
+	glDeleteProgram(CircleProgram);
+
 	glfwDestroyWindow(window);
-	/** Leállítjuk a GLFW-t. */
-	/** Stop the GLFW system. */
+
 	glfwTerminate();
-	/** Kilépés EXIT_SUCCESS kóddal. */
-	/** Stop the software and exit with EXIT_SUCCESS code. */
+
 	exit(EXIT_SUCCESS);
 }
 
+void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+	window_width	= width;
+	window_height	= height;
+
+
+	glViewport(0, 0, width, height);
+}
+
+float RandomFloat(float left_side, float right_side) {
+
+	return right_side + ((float)rand()) / (float)RAND_MAX * (left_side - right_side);
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+
+	if ((action == GLFW_PRESS) && (key == GLFW_KEY_ESCAPE))
+		cleanUpScene();
+
+	
+	if (action == GLFW_PRESS)
+		keyboard[key] = GL_TRUE;
+	else if (action == GLFW_RELEASE)
+		keyboard[key] = GL_FALSE;
+
+
+	if (key == GLFW_KEY_UP && action == GLFW_PRESS) {
+		xDir = false;
+		yDir = true;
+	}
+	
+	if (key == GLFW_KEY_DOWN && action == GLFW_PRESS) {
+		xDir = true;
+		yDir = false;
+	}
+	if (key == GLFW_KEY_S && action == GLFW_PRESS) {
+		xDir = true;
+		yDir = true;
+		x = RandomFloat(-0.90f, 0.90f);
+		y = RandomFloat(-0.90f, 0.90f);
+		random = true;
+	}
+
+}
+
+
 int main(void) {
-	/** Próbáljuk meg inicializálni a GLFW-t! */
-	/** Try to initialize GLFW! */
+
 	if (!glfwInit())
 		exit(EXIT_FAILURE);
 
-	/** A használni kívánt OpenGL verzió: 4.3. */
-	/** The needed OpenGL version: 4.3. */
+
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	//	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make macOS happy; should not be needed.
-
-		/** Próbáljuk meg létrehozni az ablakunkat. */
-		/** Let's try to create a window for drawing. */
-		/** GLFWwindow* glfwCreateWindow(int width, int height, const char* title, GLFWmonitor * monitor, GLFWwindow * share) */
+	
 	window = glfwCreateWindow(window_width, window_height, window_title, nullptr, nullptr);
 
-	/** Válasszuk ki az ablakunk OpenGL kontextusát, hogy használhassuk. */
-	/** Select the OpenGL context (window) for drawing. */
+
 	glfwMakeContextCurrent(window);
 
-	/** Incializáljuk a GLEW-t, hogy elérhetõvé váljanak az OpenGL függvények, probléma esetén kilépés EXIT_FAILURE értékkel. */
-	/** Initalize GLEW, so the OpenGL functions will be available, on problem exit with EXIT_FAILURE code. */
+
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+	glfwSetKeyCallback(window, keyCallback);
+
+
+	
 	if (glewInit() != GLEW_OK)
 		exit(EXIT_FAILURE);
 
-	/** 0 = v-sync kikapcsolva, 1 = v-sync bekapcsolva, n = n db képkockányi idõt várakozunk */
-	/** 0 = v-sync off, 1 = v-sync on, n = n pieces frame time waiting */
+	
 	glfwSwapInterval(1);
-	/** Az alkalmazáshoz kapcsolódó elõkészítõ lépések, pl. a shader objektumok létrehozása. */
-	/** The first initialization steps of the program, e.g.: creating the shader objects. */
+
+	
+	glfwSetWindowSizeLimits(window, 600, 600, 600, 600);
+	
+	glfwSetWindowAspectRatio(window, 1, 1);
+
+
 	init(window);
 
-	/** A megadott window struktúra "close flag" vizsgálata. */
-	/** Checks the "close flag" of the specified window. */
+
 	while (!glfwWindowShouldClose(window)) {
-		/** A kód, amellyel rajzolni tudunk a GLFWwindow objektumunkba. */
-		/** Call display function which will draw into the GLFWwindow object. */
-		display();
-		/** Double buffered mûködés. */
-		/** Double buffered working = swap the front and back buffer here. */
+
+		display(window, glfwGetTime());
+
 		glfwSwapBuffers(window);
-		/** Események kezelése az ablakunkkal kapcsolatban, pl. gombnyomás. */
-		/** Handle events related to our window, e.g.: pressing a key or moving the mouse. */
+
 		glfwPollEvents();
 	}
 
-	/** Felesleges objektumok törlése. */
-	/** Clenup the unnecessary objects. */
+
 	cleanUpScene();
 
-	/** Kilépés EXIT_SUCCESS kóddal. */
-	/** Stop the software and exit with EXIT_SUCCESS code. */
+
 	return EXIT_SUCCESS;
 }
--- Grafika/vertexShaderCircle.glsl
+++ Grafika/vertexShaderCircle.glsl
@@ -1,6 +1,7 @@
 #version 430 core
 layout (location = 0) in vec3 aPos;   // the position variable has attribute position 0
 layout (location = 1) in vec3 aColor; // the color variable has attribute position 1
+layout (location = 2) in vec3 bColor; // the color variable has attribute position 1
   
 out vec3 ourColor; // output a color to the fragment shader
 uniform float offsetX;
@@ -11,8 +12,6 @@ void main()
 {
     float x = aPos.x + offsetX;
 	float y = aPos.y + offsetY;
-	if(x < 0) x *= (-1.0f);
-	if(y < 0) y *= (-1.0f);
 	gl_Position = vec4(aPos.x + offsetX, aPos.y + offsetY, aPos.z, 1.0);
     ourColor = aColor; // set ourColor to the input color we got from the vertex data
 } 
